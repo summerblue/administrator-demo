2eb50160dd7fd022e5c2760dc4ee6e0ad3af9df7
Charlie Jade <cj@estgroupe.com>
2016-07-22 14:50:32 +0800

    1. setting up

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(PostTableSeeder::class);
 
         Model::reguard();
     }
